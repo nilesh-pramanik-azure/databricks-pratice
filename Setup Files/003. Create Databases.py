200a57838558e48f8be76ced61fdde535340e222
nilesh-pramanik-azure <nilesh-pramanik-azure>
2024-05-11 10:46:35 +0000

    Top 10 Dominant Drivers

--- Setup Files/003. Create Databases.py
+++ Setup Files/003. Create Databases.py
@@ -13,6 +13,11 @@
 
 # COMMAND ----------
 
+# MAGIC %sql
+# MAGIC drop database if exists f1_presentation cascade
+
+# COMMAND ----------
+
 spark.sql(f"""
 CREATE DATABASE IF NOT EXISTS f1_raw
 COMMENT 'This database will hold all the tables from raw files'
@@ -25,4 +30,12 @@ spark.sql(f"""
 CREATE DATABASE IF NOT EXISTS f1_processed
 COMMENT 'This database will hold all the tables for processed files and the tables will be Managed'
 LOCATION '/mnt/{varStorageAccntName}/{varSilverContainerName}/SQL/f1_processed_database'
+""")
+
+# COMMAND ----------
+
+spark.sql(f"""
+CREATE DATABASE IF NOT EXISTS f1_presentation
+COMMENT 'This database will hold all the tables for processed files and the tables will be Managed'
+LOCATION '/mnt/{varStorageAccntName}/{varGoldContainerName}/SQL/f1_presentation_database'
 """)
